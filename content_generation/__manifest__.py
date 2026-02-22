0a4fd844866bbfacd7b170ec0626a4ba04943860
Luka Hrachov <lukagrachov6@gmail.com>
2026-02-22 17:02:05 +0200

    added description

--- content_generation/__manifest__.py
+++ content_generation/__manifest__.py
@@ -2,5 +2,6 @@
     "name": "Content Generation",
     "author": "Luka Hrachov",
     "version": "1.0.0",
-    "depends": ["base"]
+    "depends": ["base"],
+    "description": """Module for content generation"""
 }
