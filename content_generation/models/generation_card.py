1d7a9ece9b788be2ad8b2c614126aebb3b88e16d
Luka Hrachov <lukagrachov6@gmail.com>
2026-02-23 09:10:23 +0200

    new fields added

--- content_generation/models/generation_card.py
+++ content_generation/models/generation_card.py
@@ -1,6 +1,16 @@
-from odoo import models
+from odoo import fields, models
 
 
 class GenerationCard(models.Model):
     _name = 'generation.card'
     _description = 'Generation Card'
+
+    name = fields.Char(string='Name', required=True)
+    base_language_id = fields.Many2one('res.lang', string='Base Language', required=True)
+    content_type = fields.Selection([
+        ('product', 'Product'),
+        ('set',  'Set'),
+        ('category', 'Category'),
+        ('blog', 'Blog')
+    ])
+    notes = fields.Text(string='Notes')
