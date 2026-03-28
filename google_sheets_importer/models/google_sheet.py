acb6551ab5b65e3a54a386de789e77594183f41e
Luka Hrachov <lukagrachov6@gmail.com>
2026-03-28 19:31:03 +0200

    google sheet module update

--- google_sheets_importer/models/google_sheet.py
+++ google_sheets_importer/models/google_sheet.py
@@ -6,10 +6,16 @@ class GoogleSheet(models.Model):
     _description = 'Google Sheet Import'
 
     name = fields.Char(required=True)
-    external_sheet_id = fields.Char(required=True)
+    external_sheet_id = fields.Char(required=True, copy=False)
     line_ids = fields.One2many(
         comodel_name='google.sheet.line',
         inverse_name='sheet_id',
         string='Fields',
         copy=True,
     )
+    active = fields.Boolean(default=True)
+    state = fields.Selection([
+        ('new', 'New'),
+        ('importing', 'Importing'),
+        ('done', 'Done')
+    ], default='new', required=True, copy=False, string='Status')
