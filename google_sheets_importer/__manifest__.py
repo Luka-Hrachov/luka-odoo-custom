85d0e7b88d224c4601dcfe664f9020295f430608
Luka Hrachov <lukahrachov@Lukas-MacBook-Pro.local>
2026-04-14 11:08:05 +0300

    test commit

--- google_sheets_importer/__manifest__.py
+++ google_sheets_importer/__manifest__.py
@@ -2,6 +2,7 @@
     "name": "Google Sheets Importer",
     "depends": ["base"],
     "application": True,
+    "version": "18.0.1.0.5",
     "data": [
         "security/ir.model.access.csv",
         "views/google_sheet_views.xml"
